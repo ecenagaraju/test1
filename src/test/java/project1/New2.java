ab05f4310498503172bd14b4aeba92d34358d208
ecenagaraju <46028212+ecenagaraju@users.noreply.github.com>
2025-02-22 19:47:33 +0530

    Update New2.java

--- src/test/java/project1/New2.java
+++ src/test/java/project1/New2.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+
+//////aaa
 public class New2 {
   @Test
   public void f() {
